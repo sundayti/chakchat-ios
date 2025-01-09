d5bc9998d7fe6803a803997516a484155df65ae5
mmrdr <isaev2k18@gmail.com>
2025-01-09 12:39:34 +0300

    Created worker interaction with verification service

--- chakchat/Screens/VerifyCode/VerifyWorker.swift
+++ chakchat/Screens/VerifyCode/VerifyWorker.swift
@@ -9,15 +9,32 @@ import Foundation
 import UIKit
 final class VerifyWorker: VerifyWorkerLogic {
     
+    private let verificationService: VerificationServiceLogic
     private let keychainManager: KeychainManagerBusinessLogic
+
     
-    init(keychainManager: KeychainManagerBusinessLogic) {
+    init(keychainManager: KeychainManagerBusinessLogic, verificationService: VerificationServiceLogic) {
         self.keychainManager = keychainManager
+        self.verificationService = verificationService
     }
     
     func sendRequest(_ request: Verify.SendVerifyCodeRequest,
                      completion: @escaping (Result<Void, any Error>) -> Void) {
         print("Send request to service")
+        verificationService.send(request) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success():
+                    print("Code confirmed")
+                    // Move to signup screen
+                case .failure(let error):
+                    print("Error: \(error)")
+                    completion(.failure(error))
+                    // Start to cry
+                }
+            }
+        }
+        
     }
     
     func getVerifyCode() -> UUID? {
