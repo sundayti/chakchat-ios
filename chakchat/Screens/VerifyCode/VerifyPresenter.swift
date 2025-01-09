5a17619987767920bdf4bd2c5dfcc2a137218b4c
mmrdr <isaev2k18@gmail.com>
2025-01-09 13:15:05 +0300

    Created route from verify screen to signup screen

--- chakchat/Screens/VerifyCode/VerifyPresenter.swift
+++ chakchat/Screens/VerifyCode/VerifyPresenter.swift
@@ -8,6 +8,15 @@
 import Foundation
 import UIKit
 final class VerifyPresenter: VerifyPresentationLogic {
-    
+
     weak var view: VerifyViewController?
+    
+    func routeToSignupScreen() {
+        view?.navigationController?.pushViewController(SignupAssembly.build(), animated: true)
+    }
+    
+    func showError(_ error: any Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        view?.present(alert, animated: true, completion: nil)
+    }
 }
