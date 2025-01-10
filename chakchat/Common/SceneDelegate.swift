1b29a93e31415739088c48380011960383f51fe4
mmrdr <isaev2k18@gmail.com>
2025-01-10 12:59:47 +0300

    Implemented app coordinator in start point of application(scene delegate)

--- chakchat/Common/SceneDelegate.swift
+++ chakchat/Common/SceneDelegate.swift
@@ -10,16 +10,17 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let uiwindow = UIWindow(windowScene: windowScene)
-        window = uiwindow
-        let mainVC = RegistrationAssembly.build()
-        let navC = UINavigationController(rootViewController: mainVC)
-        uiwindow.rootViewController = navC
-        uiwindow.makeKeyAndVisible()
+        window = UIWindow(windowScene: windowScene)
+        
+        appCoordinator = AppCoordinator(window: window!)
+        appCoordinator?.start()
+        
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
