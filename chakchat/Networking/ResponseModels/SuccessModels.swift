d7d4ed6af67cb89f271b19cb25375ef4b78f93c1
mmrdr <isaev2k18@gmail.com>
2025-01-12 12:09:06 +0300

    Moved all success responses models to a separate file

--- chakchat/Networking/ResponseModels/SuccessModels.swift
+++ chakchat/Networking/ResponseModels/SuccessModels.swift
@@ -9,5 +9,31 @@ import Foundation
 import UIKit
 
 enum SuccessModels {
+    struct Tokens: Codable {
+        let accessToken: String
+        let refreshToken: String
+        
+        enum CodingKeys: String, CodingKey {
+            case accessToken = "access_token"
+            case refreshToken = "refresh_token"
+        }
+    }
     
+    struct VerifySignupData: Codable {}
+    
+    struct SendCodeSigninData: Codable {
+        let signinKey: UUID
+        
+        enum CodingKeys: String, CodingKey {
+            case signinKey = "signin_key"
+        }
+    }
+    
+    struct SendCodeSignupData: Codable {
+        let signupKey: UUID
+        
+        enum CodingKeys: String, CodingKey {
+            case signupKey = "signup_key"
+        }
+    }
 }
