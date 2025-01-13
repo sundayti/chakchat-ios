c623449941f7c78c0a9743b9efbf8c527a5f522a
mmrdr <isaev2k18@gmail.com>
2025-01-13 19:01:12 +0300

    Changed regex for phone validation

--- chakchat/Validators/PhoneValidator.swift
+++ chakchat/Validators/PhoneValidator.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 class PhoneValidator: SendCodeValidator {
     func validate(_ value: String) -> Bool {
-        let phoneRegex = "^8[0-9]{10}$"
+        let phoneRegex = #"^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$"#
         let predicate = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
         return predicate.evaluate(with: value)
     }
