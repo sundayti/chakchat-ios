e3d7f87e1ac751ac1ae1aeedc10193e2ddf786b7
mmrdr <isaev2k18@gmail.com>
2025-01-15 17:23:58 +0300

    Created layout for signup screen

--- chakchat/Screens/Signup/SignupViewController.swift
+++ chakchat/Screens/Signup/SignupViewController.swift
@@ -9,11 +9,39 @@ import Foundation
 import UIKit
 final class SignupViewController: UIViewController {
     
+    enum Constants {
+        static let chakchatStackViewTopAnchor: CGFloat = 20
+        
+        static let chakLabelText: String = "Chak"
+        static let chatLabelText: String = "Chat"
+        
+        static let chakchatFont: UIFont = UIFont(name: "RammettoOne-Regular", size: 80)!
+        static let inputPhoneFont: UIFont = UIFont(name: "RobotoMono-Regular", size: 28)!
+        
+        static let chakchatStackViewSpacing: CGFloat = -50
+        
+        static let inputButtonText: String = "Enter"
+        static let inputButtonHeight: CGFloat = 50
+        static let inputButtonWidth: CGFloat = 200
+        static let inputButtonFont: UIFont = UIFont.systemFont(ofSize: 26, weight: .bold)
+        static let inputButtonTopAnchor: CGFloat = 40
+        static let inputButtonGradientColor: [CGColor] = [UIColor.yellow.cgColor, UIColor.orange.cgColor]
+        
+        static let inputButtonGradientStartPoint: CGPoint = CGPoint(x: 0.0, y: 0.5)
+        static let inputButtonGradientEndPoint: CGPoint = CGPoint(x: 1, y: 0.5)
+        static let inputButtonGradientCornerRadius: CGFloat = 25
+        
+    }
+    
     private let interactor: SignupBusinessLogic
     
+    private lazy var chakLabel: UILabel = UILabel()
+    private lazy var chatLabel: UILabel = UILabel()
+    private lazy var chakchatStackView = UIStackView(arrangedSubviews: [chakLabel, chatLabel])
     private lazy var nameTextField: UITextField = UITextField()
     private lazy var usernameTextField: UITextField = UITextField()
-    private lazy var inputButton: UIButton = UIButton(type: .system)
+    private lazy var sendButtonGradientLayer: CAGradientLayer = CAGradientLayer()
+    private lazy var sendButton: UIButton = UIButton(type: .system)
     
     private var isNameInputValid: Bool = false
     private var isUsernameInputValid: Bool = false
@@ -29,37 +57,67 @@ final class SignupViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.leftBarButtonItem?.isEnabled = false
+        let backButton = UIBarButtonItem()
+        navigationItem.leftBarButtonItem = backButton
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tapGesture.cancelsTouchesInView = false
         view.addGestureRecognizer(tapGesture)
+        
         configureUI()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // Updating the size of the gradient layer if the button has changed its size
+        sendButtonGradientLayer.frame = sendButton.bounds
     }
     
     private func configureUI() {
         view.backgroundColor = .white
+        configureChakChatStackView()
         configureNameTextField()
         configureUsernameTextField()
         configureInputButton()
     }
     
+    private func configureChakChatStackView() {
+        view.addSubview(chakLabel)
+        view.addSubview(chatLabel)
+        chakLabel.text = Constants.chakLabelText
+        chakLabel.textAlignment = .center
+        chakLabel.font = Constants.chakchatFont
+        chakLabel.textColor = .black
+        
+        chatLabel.text = Constants.chatLabelText
+        chatLabel.textAlignment = .center
+        chatLabel.font = Constants.chakchatFont
+        chatLabel.textColor = .black
+        
+        view.addSubview(chakchatStackView)
+        chakchatStackView.axis = .vertical
+        chakchatStackView.alignment = .center
+        chakchatStackView.spacing = Constants.chakchatStackViewSpacing
+        chakchatStackView.pinTop(view.safeAreaLayoutGuide.topAnchor, Constants.chakchatStackViewTopAnchor)
+        chakchatStackView.pinCentreX(view)
+    }
+    
     private func configureNameTextField() {
         view.addSubview(nameTextField)
-        nameTextField.borderStyle = UITextField.BorderStyle.bezel
+        nameTextField.borderStyle = UITextField.BorderStyle.roundedRect
         nameTextField.placeholder = "Name"
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: nameTextField.frame.height))
         nameTextField.leftView = paddingView
         nameTextField.leftViewMode = .always
         nameTextField.delegate = self
-        nameTextField.pinTop(view.safeAreaLayoutGuide.topAnchor, 60)
+        nameTextField.pinTop(chakchatStackView.bottomAnchor, 40)
         nameTextField.pinCentreX(view)
-        nameTextField.setHeight(40)
+        nameTextField.setHeight(50)
         nameTextField.setWidth(300)
     }
     
     private func configureUsernameTextField() {
         view.addSubview(usernameTextField)
-        usernameTextField.borderStyle = UITextField.BorderStyle.bezel
+        usernameTextField.borderStyle = UITextField.BorderStyle.roundedRect
         usernameTextField.placeholder = "Username"
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: usernameTextField.frame.height))
         usernameTextField.leftView = paddingView
@@ -67,20 +125,31 @@ final class SignupViewController: UIViewController {
         usernameTextField.delegate = self
         usernameTextField.pinTop(nameTextField.bottomAnchor, 20)
         usernameTextField.pinCentreX(view)
-        usernameTextField.setHeight(40)
+        usernameTextField.setHeight(50)
         usernameTextField.setWidth(300)
     }
     
     private func configureInputButton() {
-        view.addSubview(inputButton)
-        inputButton.setTitle("Sign Up", for: .normal)
-        inputButton.isEnabled = false
-        inputButton.alpha = 0.5
-        inputButton.setTitleColor(.white, for: .normal)
-        inputButton.backgroundColor = .systemBlue
-        inputButton.pinBottom(view.safeAreaLayoutGuide.bottomAnchor, 60)
-        inputButton.pinCentreX(view)
-        inputButton.addTarget(self, action: #selector(inputButtonPressed), for: .touchUpInside)
+        view.addSubview(sendButton)
+        sendButton.setTitle(Constants.inputButtonText, for: .normal)
+        sendButton.setTitleColor(.white, for: .normal)
+        sendButton.titleLabel?.font = Constants.inputButtonFont
+        sendButton.pinCentreX(view)
+        sendButton.pinTop(usernameTextField.bottomAnchor, Constants.inputButtonTopAnchor)
+        sendButton.setHeight(Constants.inputButtonHeight)
+        sendButton.setWidth(Constants.inputButtonWidth)
+        
+        sendButtonGradientLayer.colors = Constants.inputButtonGradientColor
+        sendButtonGradientLayer.startPoint = Constants.inputButtonGradientStartPoint
+        sendButtonGradientLayer.endPoint = Constants.inputButtonGradientEndPoint
+        sendButtonGradientLayer.cornerRadius = Constants.inputButtonGradientCornerRadius
+        
+        sendButton.layer.insertSublayer(sendButtonGradientLayer, at: 0)
+        sendButtonGradientLayer.frame = sendButton.bounds
+        
+        sendButton.layoutIfNeeded()
+        
+        sendButton.addTarget(self, action: #selector(sendButtonPressed), for: .touchUpInside)
     }
     
     internal func textFieldDidEndEditing(_ textField: UITextField) {
@@ -100,8 +169,7 @@ final class SignupViewController: UIViewController {
     }
     
     private func updateAuthorizationButtonState() {
-        inputButton.isEnabled = isNameInputValid && isUsernameInputValid
-        inputButton.alpha = inputButton.isEnabled ? 1.0 : 0.5
+        sendButton.isEnabled = isNameInputValid && isUsernameInputValid
     }
     
     @objc
@@ -110,7 +178,7 @@ final class SignupViewController: UIViewController {
     }
     
     @objc
-    private func inputButtonPressed() {
+    private func sendButtonPressed() {
         interactor.sendSignupRequest(nameTextField.text!, usernameTextField.text!)
     }
 }
