e9149517daddd6db4143e8e03cb8dff0963eccc1
lzkgmr <eikhromova@edu.hse.ru>
2025-01-17 12:11:31 +0300

    Used new enum error extentions in VC Presentors

--- chakchat/Screens/SendPhoneCode/SendCodePresenter.swift
+++ chakchat/Screens/SendPhoneCode/SendCodePresenter.swift
@@ -14,28 +14,9 @@ class SendCodePresenter: SendCodePresentationLogic {
     weak var view: SendCodeViewController?
     
     func showError(_ error: Error) {
-        let alert = UIAlertController(title: "Error", message: getErrorMessage(error), preferredStyle: .alert)
+        let alert = UIAlertController(title: "Error", message: error.getErrorMessage, preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alert.addAction(cancelAction)
         view?.present(alert, animated: true, completion: nil)
-    }
-
-    func getErrorMessage(_ error: Error) -> String {
-        if error is APIError {
-            return "Server error. Please send us an email with the error details to chakkchatt@yandex.ru"
-        }
-
-        if let urlError = error as? URLError {
-            switch urlError.code {
-            case .notConnectedToInternet:
-                return "No Internet connection. Try later."
-            case .timedOut:
-                return "The wait time has expired. Try again."
-            default:
-                return "Unknown error. Please send us an email with the error details to chakkchatt@yandex.ru"
-            }
-        }
-
-        return "Unknown error. Please send us an email with the error details to chakkchatt@yandex.ru"
     }
 }
