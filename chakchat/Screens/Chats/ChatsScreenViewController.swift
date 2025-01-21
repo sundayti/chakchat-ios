b93b32a6d38411e0abb55cec933e23886dda70f4
mmrdr <isaev2k18@gmail.com>
2025-01-21 15:11:35 +0300

    Created settings and new chat buttons

--- chakchat/Screens/Chats/ChatsScreenViewController.swift
+++ chakchat/Screens/Chats/ChatsScreenViewController.swift
@@ -9,6 +9,9 @@ import Foundation
 import UIKit
 final class ChatsScreenViewController: UIViewController {
     
+    private lazy var titleLabel: UILabel = UILabel()
+    private lazy var settingButton: UIButton = UIButton(type: .system)
+    private lazy var newChatButton: UIButton = UIButton(type: .system)
     var interactor: ChatsScreenBusinessLogic
     
     init(interactor: ChatsScreenBusinessLogic) {
@@ -22,6 +25,40 @@ final class ChatsScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        configureTitleLabel()
+        navigationItem.title = titleLabel.text
+        configureSettingsButton()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: settingButton)
+        configureNewChatButton()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: newChatButton)
+    }
+    
+    private func configureTitleLabel() {
+        view.addSubview(titleLabel)
+        titleLabel.font = UIFont.systemFont(ofSize: 24)
+        titleLabel.text = "Chats"
+    }
+    
+    private func configureSettingsButton() {
+        view.addSubview(settingButton)
+        settingButton.setImage(UIImage(systemName: "gearshape"), for: .normal)
+        settingButton.contentHorizontalAlignment = .fill
+        settingButton.contentVerticalAlignment = .fill
+        settingButton.setHeight(40)
+        settingButton.setWidth(40)
+    }
+    
+    private func configureNewChatButton() {
+        view.addSubview(newChatButton)
+        newChatButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        newChatButton.contentHorizontalAlignment = .fill
+        newChatButton.contentVerticalAlignment = .fill
+        newChatButton.setHeight(40)
+        newChatButton.setWidth(40)
     }
 }
