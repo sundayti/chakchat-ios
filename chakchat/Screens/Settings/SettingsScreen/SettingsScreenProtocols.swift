445ac0449dd26cc24345e0b7b4e9c6124f4edad2
mmrdr <isaev2k18@gmail.com>
2025-01-24 19:54:26 +0300

    Create new methods for loading and configuring user data

--- chakchat/Screens/Settings/SettingsScreen/SettingsScreenProtocols.swift
+++ chakchat/Screens/Settings/SettingsScreen/SettingsScreenProtocols.swift
@@ -8,9 +8,11 @@
 import Foundation
 protocol SettingsScreenBusinessLogic {
     func profileSettingsRoute()
+    func showUserData(_ data: SettingsScreenModels.UserData)
+    func loadUserData()
 }
 protocol SettingsScreenPresentationLogic {
-    
+    func showUserData(_ data: SettingsScreenModels.UserData)
 }
 protocol SettingsScreenWorkerLogic {
     
