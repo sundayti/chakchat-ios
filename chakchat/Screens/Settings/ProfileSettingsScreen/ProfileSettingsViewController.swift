f1d7880740a5d40d62ae91fa74e337697a3de180
mmrdr <isaev2k18@gmail.com>
2025-01-26 15:24:51 +0300

    Created minimal layout for profile settings screen

--- chakchat/Screens/Settings/ProfileSettingsScreen/ProfileSettingsViewController.swift
+++ chakchat/Screens/Settings/ProfileSettingsScreen/ProfileSettingsViewController.swift
@@ -11,6 +11,8 @@ final class ProfileSettingsViewController: UIViewController {
     
     private lazy var titleLabel: UILabel = UILabel()
     private lazy var iconImageView: UIImageView = UIImageView()
+    private var nicknameTextField: UITextField = UITextField()
+    private var usernameTextField: UITextField = UITextField()
     let interactor: ProfileSettingsBusinessLogic
     
     init(interactor: ProfileSettingsBusinessLogic) {
@@ -27,21 +29,32 @@ final class ProfileSettingsViewController: UIViewController {
         configureUI()
     }
     
+    public func configureUserData(_ userData: ProfileSettingsModels.ProfileUserData) {
+        configureNicknameLabel(userData.nickname)
+        configureUsernameLabel(userData.username)
+    }
+    
     private func configureUI() {
         view.backgroundColor = .white
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
         configureTitleLabel()
-        navigationItem.title = titleLabel.text
+        navigationItem.titleView = titleLabel
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelButtonPressed))
         navigationItem.leftBarButtonItem?.tintColor = .orange
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Apply", style: .plain, target: self, action: #selector(applyButtonPressed))
         navigationItem.rightBarButtonItem?.tintColor = .orange
         configureIconImageView()
+        interactor.loadUserData()
     }
     
     private func configureTitleLabel() {
         view.addSubview(titleLabel)
-        titleLabel.font = UIFont.systemFont(ofSize: 24)
+        titleLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
         titleLabel.text = "Account\nSettings"
+        titleLabel.textAlignment = .center
+        titleLabel.numberOfLines = 2
     }
     
     private func configureIconImageView() {
@@ -52,7 +65,39 @@ final class ProfileSettingsViewController: UIViewController {
         iconImageView.layer.cornerRadius = iconImageView.frame.width / 2
         iconImageView.layer.masksToBounds = true
         iconImageView.pinCenterX(view)
-        iconImageView.pinTop(titleLabel.bottomAnchor, 20)
+        iconImageView.pinTop(view.safeAreaLayoutGuide.topAnchor, 20)
+        iconImageView.image = UIImage(systemName: "person.circle")
+    }
+    
+    private func configureNicknameLabel(_ nickname: String) {
+        view.addSubview(nicknameTextField)
+        nicknameTextField.text = nickname
+        nicknameTextField.placeholder = "Nickname"
+        nicknameTextField.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        nicknameTextField.setHeight(40)
+        nicknameTextField.setWidth(300)
+        nicknameTextField.borderStyle = .roundedRect
+        nicknameTextField.pinTop(iconImageView.bottomAnchor, 30)
+        nicknameTextField.pinCenterX(view)
+    }
+    
+    private func configureUsernameLabel(_ username: String) {
+        view.addSubview(usernameTextField)
+        usernameTextField.text = username
+        usernameTextField.placeholder = "Username"
+        usernameTextField.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        usernameTextField.setHeight(40)
+        usernameTextField.setWidth(300)
+        usernameTextField.borderStyle = .roundedRect
+        usernameTextField.pinTop(nicknameTextField.bottomAnchor, 15)
+        usernameTextField.pinCenterX(view)
+    }
+    
+    private func transferUserProfileData() -> ProfileSettingsModels.ProfileUserData {
+        // if text == nil, disable apply button(in future :) )
+        let newNickname = nicknameTextField.text ?? "default"
+        let newUsername = usernameTextField.text ?? "default"
+        return ProfileSettingsModels.ProfileUserData(nickname: newNickname, username: newUsername)
     }
     
     @objc
@@ -62,6 +107,12 @@ final class ProfileSettingsViewController: UIViewController {
     
     @objc
     private func applyButtonPressed() {
-        interactor.saveNewData()
+        let newData = transferUserProfileData()
+        interactor.saveNewData(newData)
+    }
+    
+    @objc
+    private func dismissKeyboard() {
+        view.endEditing(true)
     }
 }
