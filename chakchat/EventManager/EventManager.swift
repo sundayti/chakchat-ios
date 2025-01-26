440544c23ae4acc4b712d4aa2012ee19b3eb22fe
mmrdr <isaev2k18@gmail.com>
2025-01-26 15:18:21 +0300

    Created generic version of register method

--- chakchat/EventManager/EventManager.swift
+++ chakchat/EventManager/EventManager.swift
@@ -8,10 +8,10 @@
 import Foundation
 final class EventManager: EventPublisherProtocol, EventRegistererProtocol {
     
-    var handlerDict: [ObjectIdentifier : [(any Event) -> Void]] = [:]
+    var handlerDict: [AnyHashable : [(any Event) -> Void]] = [:]
     
     func publish(event: any Event) {
-        let key = ObjectIdentifier(event.self)
+        let key = ObjectIdentifier(type(of: event))
         if let handlers = handlerDict[key] {
             for handler in handlers {
                 handler(event)
@@ -19,12 +19,17 @@ final class EventManager: EventPublisherProtocol, EventRegistererProtocol {
         }
     }
     
-    func register(eventType: any Event.Type, _ eventHandler: @escaping (any Event) -> Void) {
+    func register<T: Event>(eventType: T.Type, _ eventHandler: @escaping (T) -> Void) {
         let key = ObjectIdentifier(eventType)
+        let handler: (any Event) -> Void = { event in
+            if let typedEvent = event as? T {
+                eventHandler(typedEvent)
+            }
+        }
         if handlerDict[key] != nil {
-            handlerDict[key]?.append(eventHandler)
+            handlerDict[key]?.append(handler)
         } else {
-            handlerDict[key] = [eventHandler]
+            handlerDict[key] = [handler]
         }
     }
 }
