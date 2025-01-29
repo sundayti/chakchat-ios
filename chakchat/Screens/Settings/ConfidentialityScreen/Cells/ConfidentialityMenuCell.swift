563e00a352287c3f2752148610da5f7f86d08d9f
mmrdr <isaev2k18@gmail.com>
2025-01-29 11:03:09 +0300

    Created custom cell for confidentiality settings

---
+++ chakchat/Screens/Settings/ConfidentialityScreen/Cells/ConfidentialityMenuCell.swift
@@ -0,0 +1,68 @@
+//
+//  ConfidentialityMenuCell.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 28.01.2025.
+//
+
+import Foundation
+import UIKit
+final class ConfidentialityMenuCell: UITableViewCell {
+    
+    static let cellIdentifier = "ConfidentialityCell"
+    
+    private let settingIcon: UIImageView = UIImageView()
+    private let settingLabel: UILabel = UILabel()
+    private let settingStatus: UILabel = UILabel()
+    private var hasIcon: Bool = false
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public func configure(icon: UIImage?, title: String, status: String) {
+        settingLabel.text = title
+        settingStatus.text = status
+        if icon != nil {
+            settingIcon.image = icon
+        } else {
+            settingLabel.pinLeft(contentView.leadingAnchor, 10)
+        }
+    }
+    
+    private func configureCell() {
+        configureSettingIcon()
+        configureSettingLabel()
+        configureSettingStatus()
+    }
+    
+    private func configureSettingIcon() {
+        contentView.addSubview(settingIcon)
+        settingIcon.contentMode = .scaleAspectFill
+        settingIcon.pinLeft(contentView.leadingAnchor, 10)
+        settingIcon.pinCenterY(contentView)
+        settingIcon.setWidth(24)
+        settingIcon.setHeight(24)
+    }
+    
+    private func configureSettingLabel() {
+        contentView.addSubview(settingLabel)
+        settingLabel.tintColor = .black
+        settingLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        settingLabel.pinCenterY(contentView)
+        settingLabel.pinLeft(settingIcon.trailingAnchor, 10)
+    }
+    
+    private func configureSettingStatus() {
+        contentView.addSubview(settingStatus)
+        settingStatus.tintColor = .gray
+        settingStatus.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        settingStatus.pinCenterY(contentView)
+        settingStatus.pinRight(contentView.trailingAnchor, 10)
+    }
+}
