83e0a7ee42e5cd136149f86f31febba2734d3ec5
mmrdr <isaev2k18@gmail.com>
2025-01-29 11:04:03 +0300

    Implemented build method

--- chakchat/Screens/Settings/ConfidentialityScreen/ConfidentialityScreenAssembly.swift
+++ chakchat/Screens/Settings/ConfidentialityScreen/ConfidentialityScreenAssembly.swift
@@ -9,15 +9,15 @@ import Foundation
 import UIKit
 enum ConfidentialityScreenAssembly {
     static func build(with context: SignupContext, coordinator: AppCoordinator) -> UIViewController {
-        var presenter = ConfidentialityScreenPresenter()
-        var worker = ConfidentialityScreenWorker(userDefaultsManager: context.userDefaultManager)
-        var interactor = ConfidentialityScreenInteractor(presenter: presenter, worker: worker, eventPublisher: context.eventManager)
+        let presenter = ConfidentialityScreenPresenter()
+        let worker = ConfidentialityScreenWorker(userDefaultsManager: context.userDefaultManager)
+        let interactor = ConfidentialityScreenInteractor(presenter: presenter, worker: worker, eventPublisher: context.eventManager)
         
         interactor.onRouteToSettingsMenu = { [weak coordinator] in
             coordinator?.popScreen()
         }
         
-        var view = ConfidentialityScreenViewController(interactor: interactor)
+        let view = ConfidentialityScreenViewController(interactor: interactor)
         presenter.view = view
         return view
     }
