d89634a25f4c8d20348bfc76cf243af92f3d1f68
mmrdr <isaev2k18@gmail.com>
2025-01-30 19:57:12 +0300

    Created custom cells for profile visibility settings and exceptions

---
+++ chakchat/Screens/Settings/PhoneVisibilityScreen/Cell/ExceptionsCell.swift
@@ -0,0 +1,12 @@
+//
+//  ExceptionsCell.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 30.01.2025.
+//
+
+import Foundation
+import UIKit
+final class ExceptionsCell: UITableViewCell {
+    
+}
