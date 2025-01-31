058e9f61d663e8406a353141f869b1c96eb63dfd
mmrdr <isaev2k18@gmail.com>
2025-01-31 16:23:19 +0300

    Created exception cell

--- chakchat/Screens/Settings/PhoneVisibilityScreen/Cells/ExceptionsCell.swift
+++ chakchat/Screens/Settings/PhoneVisibilityScreen/Cells/ExceptionsCell.swift
@@ -9,4 +9,57 @@ import Foundation
 import UIKit
 final class ExceptionsCell: UITableViewCell {
     
+    static let cellIdentifier = "ExceptionCell"
+    
+    private let optionLabel: UILabel = UILabel()
+    private let chevronImageView: UIImageView = UIImageView()
+    private let addLabel: UILabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureCell()
+    }
+    
+    public func configure(title: String) {
+        optionLabel.text = title
+    }
+    
+    private func configureCell() {
+        configureOptionLabel()
+        configureChevronImageView()
+        configureAddLabel()
+    }
+    
+    private func configureOptionLabel() {
+        contentView.addSubview(optionLabel)
+        optionLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        optionLabel.textColor = .black
+        optionLabel.pinCenterY(contentView)
+        optionLabel.pinLeft(contentView.leadingAnchor, 10)
+    }
+    
+    
+    private func configureChevronImageView() {
+        contentView.addSubview(chevronImageView)
+        chevronImageView.contentMode = .scaleAspectFill
+        chevronImageView.tintColor = .gray
+        chevronImageView.image = UIImage(systemName: "chevron.right")
+        chevronImageView.pinCenterY(contentView)
+        chevronImageView.pinRight(contentView.trailingAnchor, 10)
+        chevronImageView.setWidth(12)
+        chevronImageView.setHeight(12)
+    }
+    
+    private func configureAddLabel() {
+        contentView.addSubview(addLabel)
+        addLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        addLabel.textColor = .gray
+        addLabel.text = "Add"
+        addLabel.pinRight(chevronImageView.leadingAnchor, 10)
+        addLabel.pinCenterY(contentView)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
