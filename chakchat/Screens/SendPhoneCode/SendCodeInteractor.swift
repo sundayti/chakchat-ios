9bf2582ce9e9e6d18b214c2178a7c5672b2665cb
mmrdr <isaev2k18@gmail.com>
2025-02-05 18:14:52 +0300

    Created logging on registration screens

--- chakchat/Screens/SendPhoneCode/SendCodeInteractor.swift
+++ chakchat/Screens/SendPhoneCode/SendCodeInteractor.swift
@@ -6,37 +6,42 @@
 //
 
 import Foundation
+import OSLog
 
 // MARK: - SendCodeInteractor
 class SendCodeInteractor: SendCodeBusinessLogic {
 
     // MARK: - Properties
-    private var presenter: SendCodePresentationLogic
-    private var worker: SendCodeWorkerLogic
-    private var errorHandler: ErrorHandlerLogic
-    private var state: SignupState
-    
+    private let presenter: SendCodePresentationLogic
+    private let worker: SendCodeWorkerLogic
+    private let errorHandler: ErrorHandlerLogic
+    private let state: SignupState
+    private let logger: OSLog
+
     var onRouteToVerifyScreen: ((SignupState) -> Void)?
     
     // MARK: - Initialization
     init(presenter: SendCodePresentationLogic,
          worker: SendCodeWorkerLogic,
          state: SignupState,
-         errorHandler: ErrorHandlerLogic) {
-        
+         errorHandler: ErrorHandlerLogic,
+         logger: OSLog
+    ) {
         self.presenter = presenter
         self.worker = worker
         self.state = state
         self.errorHandler = errorHandler
+        self.logger = logger
     }
     
     // MARK: - Code Request Handling
     func sendCodeRequest(_ request: SendCodeModels.SendCodeRequest) {
-        print("Send request to worker")
+        os_log("Sended get code request", log: logger, type: .info)
         worker.sendInRequest(request) { [weak self] result in
             guard let self = self else {return}
             switch result {
             case .success(let state):
+                os_log("Code received, saved", log: logger, type: .info)
                 self.successTransition(state)
             case .failure(let error):
                 let errorId = self.errorHandler.handleError(error)
@@ -48,6 +53,7 @@ class SendCodeInteractor: SendCodeBusinessLogic {
     
     // MARK: - Routing
     func successTransition(_ state: SignupState) {
+        os_log("Routed to verify screen", log: logger, type: .default)
         onRouteToVerifyScreen?(state)
     }
 }
