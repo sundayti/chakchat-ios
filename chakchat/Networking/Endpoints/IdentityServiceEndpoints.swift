c21a3f2ec26cff79f717102d6bece52e00ec38a3
mmrdr <isaev2k18@gmail.com>
2025-02-19 12:01:23 +0300

    Renamed some files

---
+++ chakchat/Networking/Endpoints/IdentityServiceEndpoints.swift
@@ -0,0 +1,8 @@
+//
+//  IdentityServiceEndpoints.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 19.02.2025.
+//
+
+import Foundation
---
+++ chakchat/Services/IdentityService/IdentityService.swift
@@ -0,0 +1,8 @@
+//
+//  IdentityService.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 19.02.2025.
+//
+
+import Foundation
