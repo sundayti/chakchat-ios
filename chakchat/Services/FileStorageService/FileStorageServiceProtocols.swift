3f9f66dac497ad273e515c3f0111ddbaa66161e1
mmrdr <isaev2k18@gmail.com>
2025-02-21 21:11:58 +0300

    Some refactoring in another services

--- chakchat/Services/FileStorageService/FileStorageServiceProtocols.swift
+++ chakchat/Services/FileStorageService/FileStorageServiceProtocols.swift
@@ -13,24 +13,24 @@ protocol FileStorageServiceProtocol {
                                _ fileName: String,
                                _ mimeType: String,
                                _ accessToken: String, 
-                               completion: @escaping (Result<SuccessModels.UploadResponse, Error>) -> Void)
+                               completion: @escaping (Result<SuccessResponse<SuccessModels.UploadResponse>, Error>) -> Void)
     
     func sendFileUploadInitRequest(_ request: FileStorageRequest.UploadInit,
                                    _ accessToken: String,
-                                   completion: @escaping (Result<SuccessModels.UploadInitResponse, Error>) -> Void)
+                                   completion: @escaping (Result<SuccessResponse<SuccessModels.UploadInitResponse>, Error>) -> Void)
     
     func sendFileUploadPartRequest(_ partNumber: Int,
                                    _ uploadID: UUID,
                                    _ fileData: Data,
-                                   _ accessToken: String, completion: @escaping (Result<SuccessModels.UploadPartResponse, Error>) -> Void)
+                                   _ accessToken: String, completion: @escaping (Result<SuccessResponse<SuccessModels.UploadPartResponse>, Error>) -> Void)
     
     func sendFileUploadAbortRequest(_ request: FileStorageRequest.UploadAbort,
                                     _ accessToken: String,
-                                    completion: @escaping (Result<SuccessModels.EmptyResponse, Error>) -> Void)
+                                    completion: @escaping (Result<SuccessResponse<SuccessModels.EmptyResponse>, Error>) -> Void)
     
     func sendFileUploadCompleteRequest(_ request: FileStorageRequest.UploadComplete,
                                        _ accessToken: String,
-                                       completion: @escaping (Result<SuccessModels.UploadResponse, Error>) -> Void)
+                                       completion: @escaping (Result<SuccessResponse<SuccessModels.UploadResponse>, Error>) -> Void)
     
-    func sendGetFileRequest(_ fileID: UUID, _ accessToken: String, completion: @escaping (Result<SuccessModels.UploadResponse, Error>) -> Void)
+    func sendGetFileRequest(_ fileID: UUID, _ accessToken: String, completion: @escaping (Result<SuccessResponse<SuccessModels.UploadResponse>, Error>) -> Void)
 }
