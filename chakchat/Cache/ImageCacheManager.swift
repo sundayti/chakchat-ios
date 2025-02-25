c121e230fc4d7e9cd654367534a5644b49458c83
mmrdr <isaev2k18@gmail.com>
2025-02-25 19:38:57 +0300

    Created cache manager

---
+++ chakchat/Cache/ImageCacheManager.swift
@@ -0,0 +1,23 @@
+//
+//  ImageCacheManager.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 24.02.2025.
+//
+
+import UIKit
+
+final class ImageCacheManager: ImageCacheProtocol {
+    
+    static let shared = ImageCacheManager()
+    
+    private let cache = NSCache<NSURL, UIImage>()
+    
+    func getImage(for url: NSURL) -> UIImage? {
+        return cache.object(forKey: url)
+    }
+    
+    func saveImage(_ image: UIImage, for url: NSURL) {
+        cache.setObject(image, forKey: url)
+    }
+}
