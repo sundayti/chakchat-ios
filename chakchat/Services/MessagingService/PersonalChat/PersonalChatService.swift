1708dc368de8ec12eda195d804473f534ec46e30
mmrdr <isaev2k18@gmail.com>
2025-02-26 12:13:31 +0300

    Created PersonalChatService

---
+++ chakchat/Services/MessagingService/PersonalChat/PersonalChatService.swift
@@ -0,0 +1,75 @@
+//
+//  PersonalChatService.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 26.02.2025.
+//
+
+import Foundation
+
+final class PersonalChatService: PersonalChatServiceProtocol {
+    func sendCreateChatRequest(
+        _ request: ChatsModels.PersonalChat.CreateRequest,
+        _ accessToken: String,
+        completion: @escaping (Result<SuccessResponse<ChatsModels.PersonalChat.Response>, any Error>) -> Void
+    ) {
+        let endpoint = MessaginServiceEndpoints.PersonalChatEndpoints.personalChat.rawValue
+        let idempotencyKey = UUID().uuidString
+        
+        let body = try? JSONEncoder().encode(request)
+        
+        let headers = [
+            "Authorization": "Bearer \(accessToken)",
+            "Idempotency-Key": idempotencyKey,
+            "Content-Type": "application/json"
+        ]
+        
+        Sender.send(endpoint: endpoint, method: .post, headers: headers, body: body, completion: completion)
+    }
+    
+    func sendBlockChatRequest(
+        _ chatID: UUID,
+        _ accessToken: String,
+        completion: @escaping (Result<SuccessResponse<ChatsModels.PersonalChat.Response>, any Error>) -> Void
+    ) {
+        let endpoint = "\(MessaginServiceEndpoints.PersonalChatEndpoints.personalChat.rawValue)/\(chatID)/block"
+        
+        let headers = [
+            "Authorization": "Bearer \(accessToken)",
+            "Content-Type": "application/json"
+        ]
+        
+        Sender.send(endpoint: endpoint, method: .put, headers: headers, completion: completion)
+    }
+    
+    func sendUnblockRequest(
+        _ chatID: UUID,
+        _ accessToken: String,
+        completion: @escaping (Result<SuccessResponse<ChatsModels.PersonalChat.Response>, any Error>) -> Void
+    ) {
+        let endpoint = "\(MessaginServiceEndpoints.PersonalChatEndpoints.personalChat.rawValue)/\(chatID)/unblock"
+        
+        let headers = [
+            "Authorization": "Bearer \(accessToken)",
+            "Content-Type": "application/json"
+        ]
+        
+        Sender.send(endpoint: endpoint, method: .delete, headers: headers, completion: completion)
+    }
+    
+    func sendDeleteChatRequest(
+        _ chatID: UUID,
+        _ deleteMode: DeleteMode,
+        _ accessToken: String,
+        completion: @escaping (Result<SuccessResponse<EmptyResponse>, any Error>) -> Void
+    ) {
+        let endpoint = "\(MessaginServiceEndpoints.PersonalChatEndpoints.personalChat.rawValue)/\(chatID)/delete/\(deleteMode)"
+        
+        let headers = [
+            "Authorization": "Bearer \(accessToken)",
+            "Content-Type": "application/json"
+        ]
+        
+        Sender.send(endpoint: endpoint, method: .delete, headers: headers, completion: completion)
+    }
+}
