90bb6d9941647dfc13272d53831c5095f8eb3886
mmrdr <isaev2k18@gmail.com>
2025-02-26 16:45:59 +0300

    Created SecretUpdateService

---
+++ chakchat/Services/MessagingService/SecretUpdate/SecretUpdateServiceProtocol.swift
@@ -0,0 +1,24 @@
+//
+//  SecretUpdateServiceProtocol.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 26.02.2025.
+//
+
+import Foundation
+
+protocol SecretUpdateServiceProtocol {
+    func sendTextMessageRequest(
+        _ request: ChatsModels.SecretUpdateModels.SendMessageRequest,
+        _ chatID: UUID,
+        _ accessToken: String,
+        completion: @escaping (Result<SuccessResponse<ChatsModels.SecretUpdateModels.SecretPreview>, Error>) -> Void
+    )
+    
+    func sendDeleteMessageRequest(
+        _ chatID: UUID,
+        _ updateID: Int64,
+        _ accessToken: String,
+        completion: @escaping (Result<SuccessResponse<EmptyResponse>, Error>) -> Void
+    )
+}
