fb0574ceb95e859d33a7fc008064d25595582cfe
mmrdr <isaev2k18@gmail.com>
2025-02-28 19:11:08 +0300

    Created CoreData models

---
+++ chakchat/CoreData/GeneratedModels/Content+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Content+CoreDataProperties.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 28.02.2025.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Content {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Content> {
+        return NSFetchRequest<Content>(entityName: "Content")
+    }
+
+    @NSManaged public var type: String?
+
+}
+
+extension Content : Identifiable {
+
+}
