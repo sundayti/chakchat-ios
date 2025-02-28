521d1f909b8da3ac0340b3768e945b654f8a55ab
mmrdr <isaev2k18@gmail.com>
2025-02-28 18:42:56 +0300

    Created CoreDataManager

---
+++ chakchat/CoreData/CoreDataManager/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 28.02.2025.
+//
+
+import Foundation
---
+++ chakchat/CoreData/CoreDataManager/CoreDataManagerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManagerProtocol.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 28.02.2025.
+//
+
+import Foundation
